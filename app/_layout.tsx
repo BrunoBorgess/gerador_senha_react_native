2e5662e5220f796f7ca40c10b2a9096d0ab0c62f
owie <lucasb.empreendimentos@gmail.com>
2024-12-02 21:19:27 -0300

    Segunda alteração

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, Image} from 'react-native'
+import { View, Text, StyleSheet, Image, TouchableOpacity} from 'react-native'
 import Slider from '@react-native-community/slider'
 
 export default function App(){
@@ -9,16 +9,25 @@ export default function App(){
           style={styles.logo}
       />
 
-      <Text style={styles.container}>20 caracteres</Text>     
+      <Text style={styles.title}>20 caracteres</Text>     
 
-      <View style={styles.container}>
+      <View style={styles.area}>
         <Slider
          style={{ height: 50}}
          minimumValue={6}
          maximumValue={20}
+         maximumTrackTintColor= '#ff0000'
+         minimumTrackTintColor='#000'
+         thumbTintColor='#392de9'
         />
+      </View>
 
-      </View> 
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Gerar Senha </Text>
+      </TouchableOpacity>
+
+
+
     </View>
   )
 }
@@ -26,11 +35,37 @@ export default function App(){
 const styles = StyleSheet.create({
   container:{
     flex:1,
-    backgroundColor:"F3F3F3",
+    backgroundColor:'F3F3FF',
     justifyContent: 'center',
     alignItems: 'center'
   },
   logo:{
     marginBottom: 60
+  },
+  area:{
+    marginTop: 14,
+    marginBottom: 14,
+    width: "80%",
+    backgroundColor: '#FFF',
+    borderRadius: 8,
+    padding: 8
+
+  },
+  button: {
+    backgroundColor: '#392de9',
+    width: '80%',
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 8,
+    marginBottom: 18
+  },
+  buttonText: {
+    color: '#FFF',
+    fontSize: 20,
+  },
+  title: {
+    fontSize: 30, 
+    fontWeight: 'bold'
   }
 })
